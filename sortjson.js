52bdbfb94a19f4bdd06c080037af09c6c1759f61
mi2ebi <merrybot47@gmail.com>
2025-09-27 20:57:53 -0700

    have a json schema

--- sortjson.js
+++ sortjson.js
@@ -1,11 +1,12 @@
 let input = "";
 process.stdin.on("data", data => { input += data; });
 process.stdin.on("end", () => {
-  const data = JSON.parse(input);
-  if (!Array.isArray(data)) {
-    console.error("uh oh the json isn't a list");
+  const wrapper = JSON.parse(input);
+  if (!wrapper.data || !Array.isArray(wrapper.data)) {
+    console.error("uh oh the json's .data isn't a list");
     process.exit(1);
   }
-  const sorted = data.sort((a, b) => a.word.localeCompare(b.word));
-  console.log(JSON.stringify(sorted, null, 2));
+  const sorted = wrapper.data.sort((a, b) => a.word.localeCompare(b.word));
+  const result = { "$schema": wrapper.schema || "./words.schema.json", "data": sorted };
+  console.log(JSON.stringify(result, null, 2));
 });
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use serde_json::Value;
 
 fn main() {
     let start = Instant::now();
-    let words =
-        serde_json::from_str::<Vec<Value>>(&fs::read_to_string("words.json").unwrap()).unwrap();
+    let json = fs::read_to_string("words.json").unwrap();
+    let wrapper = serde_json::from_str::<Value>(&json).unwrap();
+    let words = wrapper["data"].as_array().unwrap();
     let min = serde_json::to_string(&words).unwrap();
     fs::write("words.js", format!("const dict = {min};")).unwrap();
     // fonts (cf xlasisku)
